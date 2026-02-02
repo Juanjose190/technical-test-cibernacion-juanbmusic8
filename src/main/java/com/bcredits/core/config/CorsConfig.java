7729618feadbafb7e2c1162f2d157a968c6bd1c8
Juan José <86538701+Juanjose190@users.noreply.github.com>
2026-02-02 11:42:15 -0500

    Refactor CORS configuration to use allowedOrigins property

--- src/main/java/com/bcredits/core/config/CorsConfig.java
+++ src/main/java/com/bcredits/core/config/CorsConfig.java
@@ -1,27 +1,36 @@
 package com.bcredits.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
-    
+
+  
+    private String allowedOrigins;
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         
+        
+        config.setAllowedOrigins(List.of(allowedOrigins.split(",")));
+        
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        
+        config.addExposedHeader("Authorization");
+
         source.registerCorsConfiguration("/api/**", config);
+        
         return new CorsFilter(source);
     }
 }
